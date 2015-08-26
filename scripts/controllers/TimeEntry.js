7e6fab79e2c0d9b32f4069dc0b1cfd448a9cab85
Asael Chávez <asaelx@gmail.com>
2015-08-26 02:59:00 -0600

    Project structure

---
+++ scripts/controllers/TimeEntry.js
@@ -0,0 +1,15 @@
+(function(){
+
+    'use strict';
+
+    angular.module('timeTracker')
+            .controller('TimeEntry', TimeEntry);
+
+    function TimeEntry(time){
+
+        var vm = this;
+
+        vm.timeentries = [];
+    }
+
+})();
