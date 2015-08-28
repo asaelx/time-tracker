fe34599460298f837d96efaca54d73d4e6c7cb9b
Asael Chávez <asaelx@gmail.com>
2015-08-28 01:32:27 -0600

    Models updated

--- app/TimeEntry.php
+++ app/TimeEntry.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class TimeEntry extends Model
 {
     protected $table = 'time_entries';
+
+    protected $fillable = ['user_id', 'star_time', 'end_time', 'comment'];
+
+    protected $hidden = ['user_id'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
 }
